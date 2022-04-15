ca2a2e77473db4dc346f919e5add5a1ba5df35b1
Keiji Suzuki <zuki.ebetsu@gmail.com>
2022-04-15 09:42:55 +0900

    revert kernel/arch/arm64/build.mk

--- kernel/arch/arm64/build.mk
+++ kernel/arch/arm64/build.mk
@@ -8,32 +8,11 @@ CFLAGS += --target=aarch64-none-eabi -mcmodel=large
 CFLAGS += -mgeneral-regs-only
 LDFLAGS += -z max-page-size=4096
 
-QEMUFLAGS += -M raspi3 -cpu cortex-a53 -serial mon:stdio -semihosting -d guest_errors,unimp
-QEMUFLAGS +=
+QEMUFLAGS += -serial mon:stdio -semihosting -d guest_errors,unimp
 QEMUFLAGS += $(if $(GUI),,-nographic)
 QEMUFLAGS += $(if $(GDB),-S -s,)
-
-ifneq ($(E1000),)
-QEMUFLAGS += -netdev user,id=net0,hostfwd=tcp:127.0.0.1:1234-:80
-QEMUFLAGS += -object filter-dump,id=fiter0,netdev=net0,file=e1000.pcap
-QEMUFLAGS += -device e1000,netdev=net0,mac=52:54:00:12:34:56
-endif
-
-ifneq ($(VIRTIO_NET),)
-ifneq ($(VIRTIO_MODERN),)
-QEMUFLAGS += -device virtio-net,netdev=net1,packed=on
-else
-QEMUFLAGS += -device virtio-net,netdev=net1,disable-legacy=off,disable-modern=on
-endif
-QEMUFLAGS += -object filter-dump,id=fiter1,netdev=net1,file=virtio.pcap
-QEMUFLAGS += -netdev user,id=net1,hostfwd=tcp:127.0.0.1:1234-:80
-endif
-
-ifneq ($(VIRTIO_GPU),)
-QEMUFLAGS +=-vga none -device virtio-gpu,disable-legacy=off,disable-modern=on
-endif
 
 .PHONY: run
 run: $(BUILD_DIR)/kernel8.img
 	$(PROGRESS) "RUN" $<
-	$(QEMU) $(QEMUFLAGS) -kernel $< build/ide.img
+	$(QEMU) $(QEMUFLAGS) -kernel $<
