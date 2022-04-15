42364c3387ec8f4197c460af334c8579f5fe1673
Keiji Suzuki <zuki.ebetsu@gmail.com>
2022-04-15 10:02:10 +0900

    edit qemu machine name

--- kernel/arch/arm64/machines/raspi3/build.mk
+++ kernel/arch/arm64/machines/raspi3/build.mk
@@ -1,7 +1,7 @@
 objs-$(CONFIG_MACHINE_RASPI3) += boot.o peripherals.o mp.o
 
 CFLAGS += -mcpu=cortex-a53
-QEMUFLAGS += -M raspi3 -smp 4
+QEMUFLAGS += -M raspi3b -smp 4
 
 # Raspberry Pi's kernel8.img.
 .PHONY: image
