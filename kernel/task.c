93f1a72c8f3c21ae57315346ad54ccb26167670e
Keiji Suzuki <zuki.ebetsu@gmail.com>
2023-04-14 18:52:14 +0900

    taskグループをdoxygen化

--- kernel/task.c
+++ kernel/task.c
@@ -1,3 +1,4 @@
+/** @file task.c */
 #include "task.h"
 #include "ipc.h"
 #include "kdebug.h"
@@ -9,20 +10,36 @@
 #include <message.h>
 #include <string.h>
 
-/// All tasks.
+/** @ingroup task
+ * @var tasks
+ * @brief 全タスク
+ */
 static struct task tasks[CONFIG_NUM_TASKS];
-/// A queue of runnable tasks excluding currently running tasks. Lower index
-/// means higher priority.
+/** @ingroup task
+ * @var runqueues
+ * @brief 実行可能状態のタスクのキュー（現在実行中のタスクは除く）.
+ *        indexの小さいものほど優先度は高い。
+ */
 static list_t runqueues[TASK_PRIORITY_MAX];
-/// IRQ owners.
+/** @ingroup task
+ * @var irq_owners
+ * @brief IRQの所有者
+ */
 static struct task *irq_owners[IRQ_MAX];
 
+/** @ingroup task
+ * @brief タスクをランキューに入れる.
+ * @param task タスクへのポインタ
+ */
 static void enqueue_task(struct task *task) {
     list_push_back(&runqueues[task->priority], &task->runqueue_next);
 }
 
-/// Returns the task struct for the task ID. It returns NULL if the ID is
-/// invalid.
+/** @ingroup task
+ * @brief 指定されたタスクIDを持つタスク構造体を取得する（使用済みであるかチェックしない）.
+ * @param task_t タスクID
+ * @return タスク構造体へのポインタ。IDが不正であった場合は NULL.
+ */
 struct task *task_lookup_unchecked(task_t tid) {
     if (tid <= 0 || tid > CONFIG_NUM_TASKS) {
         return NULL;
@@ -31,8 +48,12 @@ struct task *task_lookup_unchecked(task_t tid) {
     return &tasks[tid - 1];
 }
 
-/// Returns the task struct for the task ID. It returns NULL if the ID is
-/// invalid or the task is not in use (TASK_UNUSED).
+/** @ingroup task
+ * @brief 指定されたタスクIDを持つタスク構造体を取得する.
+ * @param task_t タスクID
+ * @return タスク構造体へのポインタ。IDが不正である、または、タスクが
+ *         未使用(TASK_UNUSED)の場合は NULL.
+ */
 struct task *task_lookup(task_t tid) {
     struct task *task = task_lookup_unchecked(tid);
     if (!task || task->state == TASK_UNUSED) {
@@ -42,33 +63,43 @@ struct task *task_lookup(task_t tid) {
     return task;
 }
 
-/// Initializes a task and enqueue it into the run-queue.
+/** @ingroup task
+ * @brief タスクを新規作成・初期化して、ランキューに入れる.
+ * @param task タスク構造体へのポインタ
+ * @param name タスク名
+ * @param ip 実行するプログラムのエントリポイント
+ * @param pager 使用するページャへのポインタ
+ * @param flags フラグ
+ * @return 正常に処理できたら OK; それ以外は error_t 型のエラーコード.
+ */
 error_t task_create(struct task *task, const char *name, vaddr_t ip,
                     struct task *pager, unsigned flags) {
+    // 1. taskが使用済みの場合はエラー
     if (task->state != TASK_UNUSED) {
         return ERR_ALREADY_EXISTS;
     }
 
+    // 2. flagsに未知の値がないかチェック
     unsigned allowed_flags = TASK_ALL_CAPS | TASK_ABI_EMU | TASK_HV;
     if ((flags & ~allowed_flags) != 0) {
         WARN_DBG("unknown task flags (%x)", flags);
         return ERR_INVALID_ARG;
     }
-
+    // 2.1 TASK_ABI_EMUとTASK_HVは同時には指定できない
 #if defined(CONFIG_ABI_EMU) || defined(CONFIG_HYPERVISOR)
     if ((flags & (TASK_ABI_EMU | TASK_HV)) == (TASK_ABI_EMU | TASK_HV)) {
         WARN_DBG("TASK_ABI_EMU and TASK_HV are exclusive", flags);
         return ERR_INVALID_ARG;
     }
 #endif
-
+    // 2.2 CONFIG_ABI_EMUが未定義でTASK_ABI_EMUが設定されている
 #ifndef CONFIG_ABI_EMU
     if ((flags & TASK_ABI_EMU) != 0) {
         WARN_DBG("ABI emulation is not enabled");
         return ERR_UNAVAILABLE;
     }
 #endif
-
+    // 2.3 CONFIG_HYPERVISORが未定義でTASK_HVが設定されている
 #ifndef CONFIG_HYPERVISOR
     if ((flags & TASK_HV) != 0) {
         WARN_DBG("hypervisor support is not enabled");
@@ -76,13 +107,13 @@ error_t task_create(struct task *task, const char *name, vaddr_t ip,
     }
 #endif
 
-    // Do arch-specific initialization.
+    // 3. アーキテクチャ固有の初期化処理を行う
     error_t err;
     if ((err = arch_task_create(task, ip)) != OK) {
         return err;
     }
 
-    // Initialize fields.
+    // 4. task構造体のフィールドを初期化する
     TRACE("new task #%d: %s (pager=%s)", task->tid, name,
           pager ? pager->name : NULL);
     task->state = TASK_BLOCKED;
@@ -104,7 +135,7 @@ error_t task_create(struct task *task, const char *name, vaddr_t ip,
         pager->ref_count++;
     }
 
-    // Append the newly created task into the runqueue.
+    // 5. 新規作成したタスクをランキューに追加する.
     if (task != IDLE_TASK && ((flags & TASK_SCHED) == 0)) {
         task_resume(task);
     }
@@ -112,20 +143,25 @@ error_t task_create(struct task *task, const char *name, vaddr_t ip,
     return OK;
 }
 
-/// Frees the task data structures and make it unused.
+/** @ingroup task
+ * @brief タスクを破棄する.
+ * @param task タスク構造体へのポインタ
+ * @return 正常に処理できたら OK; それ以外は error_t 型のエラーコード.
+ */
 error_t task_destroy(struct task *task) {
+    // 1. カレントタスク、アイドルタスクは破棄できない
     ASSERT(task != CURRENT);
     ASSERT(task != IDLE_TASK);
-
+    // 2. initタスクは破棄できない
     if (task->tid == INIT_TASK) {
         WARN_DBG("tried to destroy the init task");
         return ERR_INVALID_ARG;
     }
-
+    // 3. 未使用タスクが指定された
     if (task->state == TASK_UNUSED) {
         return ERR_INVALID_ARG;
     }
-
+    // 4. 他のタスクから参照されている場合は破棄できない
     if (task->ref_count > 0) {
         WARN_DBG("%s (#%d) is still referenced from %d tasks", task->name,
                  task->tid, task->ref_count);
@@ -133,22 +169,26 @@ error_t task_destroy(struct task *task) {
     }
 
     TRACE("destroying %s...", task->name);
+    // 5. ランキュー、送信者キューから削除
     list_remove(&task->runqueue_next);
     list_remove(&task->sender_next);
+    // 6. アーキテクチャ固有の破棄処理を行う
     arch_task_destroy(task);
+    // 7. タスク状態を未使用に戻す
     task->state = TASK_UNUSED;
 
+    // 8. ページャから切り離す
     if (task->pager) {
         task->pager->ref_count--;
     }
 
-    // Abort sender IPC operations.
+    // 9. senderのIPC操作をすべて中止する
     LIST_FOR_EACH (sender, &task->senders, struct task, sender_next) {
         notify(sender, NOTIFY_ABORTED);
         list_remove(&sender->sender_next);
     }
 
-    // Release IRQ ownership.
+    // 10. IRQの所有権を解除する
     for (unsigned irq = 0; irq < IRQ_MAX; irq++) {
         if (irq_owners[irq] == task) {
             arch_disable_irq(irq);
@@ -159,15 +199,19 @@ error_t task_destroy(struct task *task) {
     return OK;
 }
 
-/// Exits the current task. `exp` is the reason why the task is being exited.
+/** @ingroup task
+ * @brief カレントタスクを終了させる.
+ * @param exp taskが終了した理由
+ */
 __noreturn void task_exit(enum exception_type exp) {
+    // 1. アイドルタスクは終了できない
     ASSERT(CURRENT != IDLE_TASK);
-
+    // 2. initタスクは終了できない(initタスクだけはpagerがない)
     if (!CURRENT->pager) {
         PANIC("the initial task tried to exit");
     }
 
-    // Tell its pager that this task has exited.
+    // 3. このタスクが終了したことをページャに伝える
     struct message m;
     m.type = EXCEPTION_MSG;
     m.exception.task = CURRENT->tid;
@@ -176,20 +220,26 @@ __noreturn void task_exit(enum exception_type exp) {
                       IPC_SEND | IPC_KERNEL);
     OOPS_OK(err);
 
-    // Wait until the pager task destroys this task...
+    // 4. ページャがこのタスクを破棄するまで待機する
     CURRENT->state = TASK_BLOCKED;
     CURRENT->src = IPC_DENY;
     task_switch();
     UNREACHABLE();
 }
 
-/// Suspends a task. Don't forget to update `task->src` as well!
+/** @ingroup task
+ * @brief タスクを一時停止する. `task->src` の更新も忘れないこと。
+ * @param task タスクへのポインタ
+ */
 void task_block(struct task *task) {
     DEBUG_ASSERT(task->state == TASK_RUNNABLE);
     task->state = TASK_BLOCKED;
 }
 
-/// Resumes a task.
+/** @ingroup task
+ * @brief タスクを再開する.
+ * @param task タスクへのポインタ
+ */
 void task_resume(struct task *task) {
     DEBUG_ASSERT(task->state == TASK_BLOCKED);
     task->state = TASK_RUNNABLE;
@@ -197,13 +247,20 @@ void task_resume(struct task *task) {
     mp_reschedule();
 }
 
-/// Updates the scheduling policy for the task.
+/** @ingroup task
+ * @brief タスクのスケジューリングポリシーを更新する.
+ * @param task タスクへのポインタ
+ * @param priority 新たな優先度
+ */
 error_t task_schedule(struct task *task, int priority) {
+    // 1. 優先度チェック
     if (priority >= TASK_PRIORITY_MAX) {
         return ERR_INVALID_ARG;
     }
-
+    // 2. タスク優先度を指定の優先度に変更
     task->priority = priority;
+    // 3. ランキューにある場合は、一旦削除してエンキューし直す
+    //    ランキューは優先度ごとにあるため
     if (task->state == TASK_RUNNABLE) {
         list_remove(&task->runqueue_next);
         enqueue_task(task);
@@ -212,15 +269,19 @@ error_t task_schedule(struct task *task, int priority) {
     return OK;
 }
 
-/// Picks the next task to run.
+/** @ingroup task
+ * @brief タスクスケジューラ.
+ * @param current カレントタスク
+ * @return スケジュールされたタスクへのポインタ
+ */
 static struct task *scheduler(struct task *current) {
+    // 1. カレントタスクがまだ実行可能な場合はランキューに入れる。
     if (current != IDLE_TASK && current->state == TASK_RUNNABLE) {
-        // The current task is still runnable. Enqueue into the runqueue.
         enqueue_task(current);
     }
 
-    // Look for the task with the highest priority. Tasks with the same priority
-    // is scheduled in round-robin fashion.
+    // 2. 最も優先度の高いタスクを探す。優先度が同じタスクはラウンドロビン方式で
+    //    スケジュールする。
     for (int i = 0; i < TASK_PRIORITY_MAX; i++) {
         struct task *next =
             LIST_POP_FRONT(&runqueues[i], struct task, runqueue_next);
@@ -232,99 +293,133 @@ static struct task *scheduler(struct task *current) {
     return IDLE_TASK;
 }
 
-/// Do a context switch: save the current register state on the stack and
-/// restore the next thread's state.
+/** @ingroup task
+ * @brief コンテキストスイッチを行う. 現在のコンテキストをスタックに保存して
+ *        次のスレッドのコンテキストを復元する。
+ */
 void task_switch(void) {
+    // 1. カレントタスクがスタックを使い尽くしていないかチェックする
     stack_check();
-
+    // 2. 次に実行するタスクを求める
     struct task *prev = CURRENT;
     struct task *next = scheduler(prev);
+    // 3. 次に実行するタスクのquantumをセットする
     next->quantum = TASK_TIME_SLICE;
+    // 4. カレントタスク以外に実行可能なタスクがない場合はカレントタスクを
+    //    引き続き実行する
     if (next == prev) {
-        // No runnable threads other than the current one. Continue executing
-        // the current thread.
         return;
     }
-
+    // 5. アーキテクチャ固有の実際の切り替え処理を行う
     CURRENT = next;
     arch_task_switch(prev, next);
-
+    // 6. 切り替えたタスクがスタックを使い尽くしていないかチェックする
     stack_check();
 }
 
-/// Starts receiving notifications by IRQs.
+/** @ingroup task
+ * @brief IRQからの通知の受信を開始する.
+ * @param task タスクへのポインタ
+ * @param irq 通知を受信するirq番号
+ */
 error_t task_listen_irq(struct task *task, unsigned irq) {
+    // 1. irqパラメタのチェック
     if (irq >= IRQ_MAX) {
         return ERR_INVALID_ARG;
     }
-
+    // 2. irqはすでに他に所有者がいる
     if (irq_owners[irq]) {
         return ERR_ALREADY_EXISTS;
     }
-
+    // 3. irqの所有者となる
     irq_owners[irq] = task;
+    // 4. irqを有効にする
     arch_enable_irq(irq);
     TRACE("enabled IRQ: task=%s, vector=%d", task->name, irq);
     return OK;
 }
 
-/// Stops receiving notifications by IRQs.
+/** @ingroup task
+ * @brief IRQからの通知の受信を停止する.
+ * @param irq 通知を受信しているirq番号
+ */
 error_t task_unlisten_irq(unsigned irq) {
     if (irq >= IRQ_MAX) {
         return ERR_INVALID_ARG;
     }
-
+    // 1. irqを無効にする
     arch_disable_irq(irq);
+    // 2. irqの所有権を手放す
     irq_owners[irq] = NULL;
     TRACE("disabled IRQ: vector=%d", irq);
     return OK;
 }
 
-/// Maps a memory page in the task's virtual memory space. `kpage` is a memory
-/// page which provides a memory page for arch-specific page table structures.
+/** @ingroup task
+ * @brief タスクの仮想メモリ空間にメモリページをマップする.
+ * @param task タスクへのポインタ
+ * @param vaddr 仮想アドレス
+ * @param paddr 物理アドレス
+ * @param kpage アーキテクチャ固有のページテーブル構造体用のメモリページを与えるメモリページ
+ * @param flags フラグ
+ * @return マッピングに成功したら OK; それ以外は error_t 型のエラーコード
+ */
 __mustuse error_t vm_map(struct task *task, vaddr_t vaddr, paddr_t paddr,
                          paddr_t kpage, unsigned flags) {
+    // 1. 各アドレスはページサイズにアライメントされていること
     DEBUG_ASSERT(IS_ALIGNED(vaddr, PAGE_SIZE));
     DEBUG_ASSERT(IS_ALIGNED(paddr, PAGE_SIZE));
     DEBUG_ASSERT(IS_ALIGNED(kpage, PAGE_SIZE));
 
-    // Prevent corrupting kernel memory. Note that the user is still able to
-    // bypass this check to access the kernel memory by mapping the page table
-    // structures.
+    // 2. カーネルメモリの破損を防ぐ。vaddrがカーネルメモリ領域にある場合はエラー。
+    //    ページテーブル構造体をマッピングすることによりユーザは依然として
+    //    このチェックをバイパスしてカーネルメモリにアクセスできることに注意されたい。
     if (is_kernel_addr_range(vaddr, PAGE_SIZE)) {
         WARN_DBG("vaddr %p points to a kernel memory area", vaddr);
         return ERR_NOT_ACCEPTABLE;
     }
 
-    // Looks that the mapping is not malicious. Update the page table.
+    // 3. マッピングが悪意のないものであることをチェックしてページテーブルを更新する。
     return arch_vm_map(task, vaddr, paddr, kpage, flags);
 }
 
-/// Unmaps a memory page from the task's virtual memory space.
+/** @ingroup task
+ * @brief タスクの仮想メモリ空間からメモリページをアンマップする.
+ * @param task タスクへのポインタ
+ * @param vaddr 仮想アドレス
+ * @return マッピングに成功したら OK; それ以外は error_t 型のエラーコード
+ */
 error_t vm_unmap(struct task *task, vaddr_t vaddr) {
+    // 1. アドレスはページサイズにアライメントされていること
     DEBUG_ASSERT(IS_ALIGNED(vaddr, PAGE_SIZE));
 
+    // 2. 仮想アドレスがマップされているか確認する
     paddr_t paddr = vm_resolve(task, vaddr);
     if (!paddr) {
         return ERR_NOT_FOUND;
     }
 
+    // 3. アーキテクチャ固有のアンマップ処理を行う
     return arch_vm_unmap(task, vaddr);
 }
 
-/// Handles timer interrupts. The timer fires this handler every 1/TICK_HZ
-/// seconds.
+/** @ingroup task
+ * @brief タイマー割り込みを処理する. タイマーは 1/TICK_HZ 毎に発火する.
+ */
 void handle_timer_irq(void) {
     bool resumed_by_timeout = false;
+    // 1. CPUがBSPの場合のみ処理する
     if (mp_is_bsp()) {
-        // Handle task timeouts.
+        // 2. タイムアウトを処理する
         for (int i = 0; i < CONFIG_NUM_TASKS; i++) {
             struct task *task = &tasks[i];
+            // 2.1 未使用またはtimerが設定されていない場合は読み飛ばす
             if (task->state == TASK_UNUSED || !task->timeout) {
                 continue;
             }
-
+            // 2.2 経過時間を差し引く
             task->timeout--;
+            // 2.3 タイムアウト時間になったら通知する
             if (!task->timeout) {
                 notify(task, NOTIFY_TIMER);
                 resumed_by_timeout = true;
@@ -332,7 +427,7 @@ void handle_timer_irq(void) {
         }
     }
 
-    // Switch task if the current task has spend its time slice.
+    // 3. カレントタスクがquantumを使い尽くしたらタスクスイッチを行う
     DEBUG_ASSERT(CURRENT == IDLE_TASK || CURRENT->quantum >= 0);
     CURRENT->quantum--;
     if (CURRENT->quantum < 0 || (CURRENT == IDLE_TASK && resumed_by_timeout)) {
@@ -341,10 +436,16 @@ void handle_timer_irq(void) {
 }
 
 /// Handles interrupts except the timer device used in the kernel.
+/** @ingroup task
+ * @brief カーネルで使用されているタイマーデバイスの割り込み例外を処理する.
+ * @param irq 割り込み番号
+ */
 void handle_irq(unsigned irq) {
     struct task *owner = irq_owners[irq];
+    // 1. irqの所有者に割り込みを通知する。
     if (owner) {
         notify(owner, NOTIFY_IRQ);
+        // 2. カレントタスクがアイドルタスクだったらタスクを切り替える
         if (CURRENT == IDLE_TASK) {
             task_switch();
         }
@@ -352,11 +453,20 @@ void handle_irq(unsigned irq) {
 }
 
 /// The page fault handler. It calls a pager to ask to update the page table.
+/** @ingroup task
+ * @brief ページフォルトを処理する. ページャを呼び出してページテーブルを
+ *        更新するよう依頼する。
+ * @param addr フォルトメモリアドレス
+ * @param ip フォルトが発生したプログラムアドレス
+ * @param fault フォルト原因
+ */
 void handle_page_fault(vaddr_t addr, vaddr_t ip, unsigned fault) {
+    // 1. initタスクでフォルトが発生したらpanic
     if (CURRENT->pager == NULL) {
         PANIC("page fault in the init task: addr=%p, ip=%p", addr, ip);
     }
 
+    // 2. ページャに送信するメッセージを作成する
     struct message m;
     m.type = PAGE_FAULT_MSG;
     m.page_fault.task = CURRENT->tid;
@@ -365,12 +475,15 @@ void handle_page_fault(vaddr_t addr, vaddr_t ip, unsigned fault) {
     m.page_fault.fault = fault;
     error_t err = ipc(CURRENT->pager, CURRENT->pager->tid,
                       (__user struct message *) &m, IPC_CALL | IPC_KERNEL);
+    // 3. エラーが発生した場合はタスクを終了させる
     if (err != OK || m.type != PAGE_FAULT_REPLY_MSG) {
         task_exit(EXP_INVALID_MSG_FROM_PAGER);
     }
 }
 
-/// Prints the task states. Used for debugging.
+/** @ingroup task
+ * @brief タスクの状態をプリントする. デバックで使用する。
+ */
 void task_dump(void) {
     const char *states[] = {
         [TASK_UNUSED] = "unused",
@@ -395,17 +508,20 @@ void task_dump(void) {
     }
 }
 
-/// Initializes the task subsystem.
+/** @ingroup task
+ * @brief タスクサブシステムを初期化する.
+ */
 void task_init(void) {
+    // 1. ランキューを初期化する
     for (int i = 0; i < TASK_PRIORITY_MAX; i++) {
         list_init(&runqueues[i]);
     }
-
+    // 2. タスク配列を初期化する
     for (int i = 0; i < CONFIG_NUM_TASKS; i++) {
         tasks[i].state = TASK_UNUSED;
         tasks[i].tid = i + 1;
     }
-
+    // 3. irq所有者配列を初期化する
     for (int i = 0; i < IRQ_MAX; i++) {
         irq_owners[i] = NULL;
     }
--- kernel/task.h
+++ kernel/task.h
@@ -1,3 +1,4 @@
+/** @file kernel/task.h */
 #ifndef __TASK_H__
 #define __TASK_H__
 
@@ -8,95 +9,124 @@
 #include <message.h>
 #include <types.h>
 
-/// The context switching time slice (# of ticks).
+/** @ingroup task
+ * @def TASK_TIME_SLICE
+ * @brief コンテキストスイッチを行うタイムスライス(tick数).
+ */
 #define TASK_TIME_SLICE ((CONFIG_TASK_TIME_SLICE_MS * TICK_HZ) / 1000)
 STATIC_ASSERT(TASK_TIME_SLICE > 0);
 
-//
-// Task states.
-//
-
-/// The task struct is not being used.
+/** @ingroup task
+ * @def TASK_UNUSED
+ * @brief タスクステータス: 未使用
+ */
 #define TASK_UNUSED 0
-/// The task is running or is queued in the runqueue.
+/** @ingroup task
+ * @def TASK_RUNNABLE
+ * @brief タスクステータス: 実行中、またはランキューにある
+ */
 #define TASK_RUNNABLE 1
-/// The task is waiting for a receiver/sender task in IPC.
+/** @ingroup task
+ * @def TASK_BLOCKED
+ * @brief タスクステータス: IPCで送受信タスクを待機中
+ */
 #define TASK_BLOCKED 2
 
+/** @ingroup task
+ * @def TASK_PRIORITY_MAX
+ * @brief 最大優先度
+ */
 #define TASK_PRIORITY_MAX 8
 STATIC_ASSERT(TASK_PRIORITY_MAX > 0);
 
-// struct arch_cpuvar *
+/** @ingroup task
+ * @def ARCH_CPUVAR
+ * @brief arch_cpuvar構造体
+ */
 #define ARCH_CPUVAR (&get_cpuvar()->arch)
 
-/// The current task of the current CPU (`struct task *`).
+/** @ingroup task
+ * @def CURRENT
+ * @brief カレントCPUのカレントタスク (`struct task *`)
+ */
 #define CURRENT (get_cpuvar()->current_task)
-/// The idle task of the current CPU (`struct task *`).
+
+/** @ingroup task
+ * @def IDLE_TASK
+ * @brief カレントCPUのアイドルタスク (`struct task *`)
+ */
 #define IDLE_TASK (&get_cpuvar()->idle_task)
 
-// Task capabilities.
+/** @ingroup task
+ * @def CAP_TASK
+ * @brief タスクケーパビリティ: TASK
+ */
 #define CAP_TASK   0
+/** @ingroup task
+ * @def CAP_IRQ
+ * @brief タスクケーパビリティ: IRQ
+ */
 #define CAP_IRQ    1
+/** @ingroup task
+ * @def CAP_IO
+ * @brief タスクケーパビリティ: IO
+ */
 #define CAP_IO     2
+/** @ingroup task
+ * @def CAP_MAP
+ * @brief タスクケーパビリティ: MAP
+ */
 #define CAP_MAP    3
+/** @ingroup task
+ * @def CAP_KDEBUG
+ * @brief タスクケーパビリティ: KDEBUG
+ */
 #define CAP_KDEBUG 4
+/** @ingroup task
+ * @def CAP_MAX
+ * @brief 最大タスクケーパビリティ
+ */
 #define CAP_MAX    4
 
+/** @ingroup task
+ * @def CAPABLE
+ * @brief taskはcapを実行できるか（関数）
+ */
 #define CAPABLE(task, cap)                                                     \
     (bitmap_get((task)->caps, sizeof((task)->caps), cap) != 0)
 
-/// The task struct (so-called Task Control Block).
+/** @ingroup task
+ * @struct task
+ * @brief タスク構造体（いわゆる、タスク制御ブロック）.
+ */
 struct task {
-    /// arch固有のフィールド
-    struct arch_task arch;
-    /// タスクID （1 から始まる）
-    task_t tid;
-    /// 状態
-    int state;
-    /// タスク名（NULL終端）
-    char name[CONFIG_TASK_NAME_LEN];
-    /// フラグ
-    unsigned flags;
-    /// このタスクを参照しているタスクの数
-    unsigned ref_count;
-    /// ページャタスク。ページフォルトや例外（ゼロ除算など）が発生すると
-    /// カーネルはこのページ兄メッセージを送信して、フォルトを解決させる
-    /// （または、タスクをkillする）
-    struct task *pager;
-    /// タイムスライスの残り（ティック単位）。この値が0になったら、カーネルはT
-    /// 次のタスクに切り替える（いわゆるプリエンプティブコンテキストスイッチ）
-    int quantum;
-    /// タスク優先度。小さい値ほど高い優先でを意味する。スケジューラは常に最高の
-    /// 優先度を持つrunnableなタスクを選択する。最高の優先度を持つrunnableタスクが
-    /// 複数ある場合、カーネルはラウンドロビン方式でスケジュールする。
-    int priority;
-    /// メッセージバッファ
-    struct message m;
-    /// 受け入れ可能な送り手ID。IPC_ANYの場合はアスクはすべてのタスクからの
-    /// メッセージを受け入れる。
-    task_t src;
-    /// 保留中の通知。タスクがそれをメッセージ（NOTEIFACTIONS_MSG）として
-    /// 受信するとクリアされる。
-    notifications_t notifications;
-    /// IPCタイムアウト（ミリ秒単位）。0になるとカーネルはタスクに`NOTIFY_TIMER`を
-    /// 通知する。
-    msec_t timeout;
-    /// このタスクのメッセージを受信できるようになるのを待っているタスクのキュー。
-    /// このタスクが準備できた場合、このキューにあるすべてのスレッドを再開させる。
-    list_t senders;
-    /// runキューの（intrusiveな）リスト要素。
-    list_elem_t runqueue_next;
-    /// senderキューの（intrusiveな）リスト要素。
-    list_elem_t sender_next;
-    /// ケーパビリティ（ビットマップ）
-    uint8_t caps[BITMAP_SIZE(CAP_MAX)];
+    struct arch_task arch;  /**< arch固有のフィールド */
+    task_t tid;             /**< タスクID （1 から始まる） */
+    int state;              /**< 状態 */
+    char name[CONFIG_TASK_NAME_LEN];    /**< タスク名（NULL終端） */
+    unsigned flags;         /**< フラグ */
+    unsigned ref_count;     /**< このタスクを参照しているタスクの数 */
+    struct task *pager;     /**< ページャタスク. ページフォルトや例外（ゼロ除算など）が発生するとカーネルはこのページャにメッセージを送信して、フォルトを解決させる（または、タスクをkillする） */
+    int quantum;            /**< タイムスライスの残り（ティック単位）. この値が0になったら、カーネルは次のタスクに切り替える（いわゆるプリエンプティブコンテキストスイッチ） */
+    int priority;           /**< タスク優先度. 小さい値ほど高い優先でを意味する。スケジューラは常に最高の優先度を持つrunnableなタスクを選択する。最高の優先度を持つrunnableタスクが複数ある場合、カーネルはラウンドロビン方式でスケジュールする。 */
+    struct message m;       /**< メッセージバッファ */
+    task_t src;             /**< 受け入れ可能な送信元のタスクID. IPC_ANYの場合、タスクはすべてのタスクからのメッセージを受け入れる。*/
+    notifications_t notifications;  /**< 保留中の通知. タスクがそれをメッセージ（NOTEIFACTIONS_MSG）として受信するとクリアされる。 */
+    msec_t timeout;         /**< IPCタイムアウト（ミリ秒単位）. 0になるとカーネルはタスクに`NOTIFY_TIMER`を通知する。 */
+    list_t senders;         /**< このタスクのメッセージが受信可能になるのを待機しているタスクのキュー. このタスクが準備できた場合、このキューにあるすべてのスレッドを再開させる。 */
+    list_elem_t runqueue_next;  /**< runキューの（intrusiveな）リスト要素 */
+    list_elem_t sender_next;    /**< senderキューの（intrusiveな）リスト要素 */
+    uint8_t caps[BITMAP_SIZE(CAP_MAX)]; /**< ケーパビリティ（ビットマップ） */
 };
 
-/// CPU-local variables.
+/** @ingroup task
+ * @struct cpuvar
+ * @brief CPUローカルな変数.
+ */
 struct cpuvar {
-    struct arch_cpuvar arch;
-    struct task *current_task;
-    struct task idle_task;
+    struct arch_cpuvar arch;        /**< arch_cpuvar構造体 */
+    struct task *current_task;      /**< 現在実行中のタスクへのポインタ */
+    struct task idle_task;          /**< アイドルタスク */
 };
 
 __mustuse error_t task_create(struct task *task, const char *name, vaddr_t ip,
@@ -120,8 +150,9 @@ void handle_page_fault(vaddr_t addr, vaddr_t ip, unsigned fault);
 void task_dump(void);
 void task_init(void);
 
-// Implemented in arch.
-/// in kernel/arch/<arch>/mp.c
+// アーキテクチャ固有の実装
+
+/// kernel/arch/<arch>/mp.c で実装
 void lock(void);
 void panic_lock(void);
 void unlock(void);
@@ -129,14 +160,14 @@ void mp_start(void);
 int mp_self(void);
 int mp_num_cpus(void);
 void mp_reschedule(void);
-/// in kernel/arch/<arch>/task.c
+/// kernel/arch/<arch>/task.c で実装
 __mustuse error_t arch_task_create(struct task *task, vaddr_t ip);
 void arch_task_destroy(struct task *task);
 void arch_task_switch(struct task *prev, struct task *next);
-/// in kernel/arch/x86/interrupt.c, kernel/arch/arm64/macies/raspi3/peripherals.c
+/// kernel/arch/x86/interrupt.c, kernel/arch/arm64/macies/raspi3/peripherals.c で実装
 void arch_enable_irq(unsigned irq);
 void arch_disable_irq(unsigned irq);
-/// in kernel/arch/<arch>/vm.c
+/// kernel/arch/<arch>/vm.c で実装
 __mustuse error_t arch_vm_map(struct task *task, vaddr_t vaddr, paddr_t paddr,
                               paddr_t kpage, unsigned flags);
 __mustuse error_t arch_vm_unmap(struct task *task, vaddr_t vaddr);
