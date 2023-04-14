6e6a5afe42bd3e848fc541b8c8a207f67f76ec40
Keiji Suzuki <zuki.ebetsu@gmail.com>
2023-04-14 18:51:51 +0900

    kernelグループをtaskとipcに分解

--- kernel/Doxygroup.c
+++ kernel/Doxygroup.c
@@ -2,6 +2,14 @@
  * @defgroup kernel カーネル
  * @brief Resea OSのカーネル
  *
+ * @defgroup task カーネルタスク
+ * @ingroup kernel
+ * @brief カーネルタスクの実装
+ *
+ * @defgroup ipc IPC
+ * @ingroup kernel
+ * @brief タスク間のメッセージ送受信機能の実装
+ *
  * @defgroup arm64 arm64カーネル
  * @ingroup kernel
  * @brief arm64固有のカーネル関数
