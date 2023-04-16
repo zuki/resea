ca5a1448311c31aaa52b19b67d911f34925363fc
Keiji Suzuki <zuki.ebetsu@gmail.com>
2023-04-16 14:45:03 +0900

    serversのグループ名を修正

--- servers/Doxygroup.c
+++ servers/Doxygroup.c
@@ -2,48 +2,48 @@
  * @defgroup servers サーバ
  * @brief 各種サーバの実装
  *
- * @defgroup vm vmサービス
+ * @defgroup vm 仮想メモリ管理
  * @ingroup servers
- * @brief 仮想メモリ(vm)の実装
+ * @brief 仮想メモリ(vm)管理サーバの実装
  *
- * @defgroup dm dmサービス
+ * @defgroup dm デバイス管理
  * @ingroup servers
  * @brief デバイス管理(dm)の実装
  *
- * @defgroup apps appsサービス
+ * @defgroup apps アプリケーション
  * @ingroup servers
  * @brief 各種アプリケーションの実装
  *
- * @defgroup drivers driversサービス
+ * @defgroup drivers ドライバ
  * @ingroup servers
  * @brief 各種ドライバの実装
  *
- * @defgroup fs fsサービス
+ * @defgroup fs ファイルシステム
  * @ingroup servers
  * @brief ファイルシステムの実装
  *
- * @defgroup hv hvサービス
+ * @defgroup hv ハイパーバイザ
  * @ingroup servers
- * @brief ハイパバイザの実装
+ * @brief ハイパーバイザの実装
  *
- * @defgroup minlin minlinサービス
+ * @defgroup minlin MinLin
  * @ingroup servers
  * @brief Linux ABIエミュレーション(MinLin)の実装
  *
- * @defgroup random randomサービス
+ * @defgroup random 乱数発生
  * @ingroup servers
- * @brief 乱数の実装
+ * @brief 乱数発生の実装
  *
- * @defgroup shell shellサービス
+ * @defgroup shell Shell
  * @ingroup servers
  * @brief shellの実装
  *
- * @defgroup tcpip tcpipサービス
+ * @defgroup tcpip TCP/IPスタック
  * @ingroup servers
  * @brief TCP/IPプロトコルスタックの実装
  *
- * @defgroup experimental experimentalサービス
+ * @defgroup experimental 実験サーバ
  * @ingroup servers
- * @brief 各種実験的サービスの実装
+ * @brief 各種実験的サーバの実装
  *
  */
